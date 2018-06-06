224589efa928f70700d564a32a58ae1dd55108be
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-06 14:35:51 +0200

    Tidy up get screenshot method

--- screenshot.py
+++ screenshot.py
@@ -4,25 +4,36 @@ import time
 
 import json
 
-with open('brightconfig.json') as f:
-    data = json.load(f)
 
-browser = webdriver.Chrome()
-browser.get("https://scicatapi.esss.dk/visualize")
-browser.save_screenshot("visualize.png")
-browser.get("https://scicatapi.esss.dk/modeldiagram")
-browser.save_screenshot("modeldiagram.png")
+class GetScreenshot():
+    def __init__(self):
+        x = 2
 
-browser.get("https://scicat.esss.dk/login")
-browser.save_screenshot("login.png")
-username = browser.find_element_by_id("emailInput")
-password = browser.find_element_by_id("pwdInput")
+    def get_screenshot(self):
+        with open('brightconfig.json') as f:
+            data = json.load(f)
 
-username.send_keys(data["username"])
-password.send_keys(data["password"])
+        browser = webdriver.Chrome()
+        browser.get("https://scicatapi.esss.dk/visualize")
+        browser.save_screenshot("visualize.png")
+        browser.get("https://scicatapi.esss.dk/modeldiagram")
+        browser.save_screenshot("modeldiagram.png")
 
-element = browser.find_element_by_id("login-btn").click()
-time.sleep(10)
-browser.save_screenshot("dashboard.png")
+        browser.get("https://scicat.esss.dk/login")
+        browser.save_screenshot("login.png")
+        username = browser.find_element_by_id("emailInput")
+        password = browser.find_element_by_id("pwdInput")
 
-browser.quit()
+        username.send_keys(data["username"])
+        password.send_keys(data["password"])
+
+        element = browser.find_element_by_id("login-btn").click()
+        time.sleep(10)
+        browser.save_screenshot("dashboard.png")
+
+        browser.quit()
+
+
+if __name__ == '__main__':
+    g= GetScreenshot()
+    g.get_screenshot()
