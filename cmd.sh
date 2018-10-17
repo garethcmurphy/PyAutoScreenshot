f5877976d620e23cc41bb2df805c30aa72757652
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-17 05:25:21 +0200

    add chromedriver

---
+++ cmd.sh
@@ -0,0 +1,2 @@
+export PATH=$PATH:./chromedriver
+pipenv run ./PyAutoScreenshot.py
---
+++ get.sh
@@ -0,0 +1,4 @@
+wget https://chromedriver.storage.googleapis.com/2.42/chromedriver_mac64.zip
+mv chromedriver_mac64.zip chromedriver
+cd chromedriver
+unzip chromedriver_mac64.zip
